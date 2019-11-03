9dfadc07492316ecb95c8663e82cb493c3053613
razamanaza <razamanaza@yandex.ru>
2019-11-03 08:18:03 +0000

    Finished sleep model, migration, seeding

--- app/User.php
+++ app/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Workout');
     }
+
+    public function sleeps()
+    {
+        return $this->hasMany('App\Sleep');
+    }
 }
--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ php artisan db:seed --class=UsersTableSeeder
 php artisan db:seed --class=TargetsTableSeeder
 php artisan db:seed --class=WorkoutTypesTableSeeder
 php artisan db:seed --class=WorkoutsTableSeeder
+php artisan db:seed --class=SleepsTableSeeder
